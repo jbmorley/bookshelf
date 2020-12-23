173a772d98a4f81a8c7481403a7c7482bacb6077
Jason Barrie Morley <jbmorley@mac.com>
2020-12-23 10:19:45 -0800

    Initial work to move the add book functionality into the main bookshelf script.

--- bookshelf.py
+++ bookshelf.py
@@ -13,17 +13,19 @@ import frontmatter
 import pick
 
 import books
+import googlebooks
 import utilities
+
+
+class AddBookInterrupt(Exception):
+    pass
 
 
 def signal_handler(sig, frame):
     sys.exit(0)
 
 
-def main():
-    parser = argparse.ArgumentParser(description="Book tracker.")
-    options = parser.parse_args()
-
+def interactive_books(directory):
     statuses = list(books.Status)
 
     def next_shelf(picker):
@@ -42,19 +44,38 @@ def main():
         selected.status = statuses[new_status_index]
         selected.save()
 
+    def add_book(picker):
+        return None, -2
+
     def cancel(picker):
         exit(0)
 
     signal.signal(signal.SIGINT, signal_handler)
     utilities.set_escdelay(25)
-    directory = books.library_path()
     picker = utilities.SearchablePicker(options=books.load(directory),
                                         title="Books (all shelves)",
                                         options_map_func=lambda x: x.summary)
     picker.register_custom_handler(curses.KEY_LEFT, previous_shelf)
     picker.register_custom_handler(curses.KEY_RIGHT, next_shelf)
+    picker.register_custom_handler(ord('\t'), add_book)
     picker.register_custom_handler(27, cancel)
-    option, index = picker.start()
+
+    book, index = picker.start()
+    if index == -2:
+        raise AddBookInterrupt()
+    return book
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Book tracker.")
+    options = parser.parse_args()
+
+    directory = books.library_path()
+    while True:
+        try:
+            interactive_books(directory=directory)
+        except AddBookInterrupt:
+            books.add_book(directory=directory, search_callback=googlebooks.search)
 
 
 if __name__ == "__main__":
