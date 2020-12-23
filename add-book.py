16afa761baee53b11d3f20f893426bf29eb721cd
Jason Barrie Morley <jbmorley@mac.com>
2020-12-23 08:17:02 -0800

    Introduce an environment variable for specifying the library location.

--- add-book.py
+++ add-book.py
@@ -13,10 +13,13 @@ BOOKS_DIRECTORY = "~/Projects/jbmorley.co.uk/content/about/books/"
 
 
 def main():
+    try:
+        directory = os.path.expanduser(os.environ["BOOKSHELF_LIBRARY_PATH"])
+    except KeyError:
+        exit("Use the BOOKSHELF_LIBRARY_PATH environment variable to specify the location of your library.")
     parser = argparse.ArgumentParser(description="Add a new book")
     options = parser.parse_args()
-    utilities.add_book(directory=os.path.expanduser(BOOKS_DIRECTORY),
-                       search_callback=googlebooks.search)
+    utilities.add_book(directory=directory, search_callback=googlebooks.search)
 
 
 if __name__ == "__main__":
